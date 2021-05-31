63058fe32b829f3acc6460cb7d6d3d767649a52a
laerpeeK <847749593@qq.com>
2021-06-01 02:21:59 +0800

    setter

---
+++ learnJS/MDN/setter.js
@@ -0,0 +1,25 @@
+/*
+当尝试设置属性时，set语法将对象属性绑定到要调用的函数。
+ */
+
+const language = {
+    set current(name) {
+        this.log.push(name)
+    },
+    log: [],
+    a:0,
+    get sayLog() {
+        console.log(this.log)
+    }
+}
+language.current = 'EN'
+language.current = 'FA'
+
+language.sayLog
+
+Object.defineProperty(language,'b' ,{set: function(x){
+        this.a = x/2
+    }})
+language.b = 10
+console.log(language.a) //5
+
