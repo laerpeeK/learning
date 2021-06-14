8a0930897e9d191219fe5c5a10d7aeafe3e071d8
laerpeeK <847749593@qq.com>
2021-06-14 09:23:25 +0800

    learnWebpack from bilibili尚硅谷视频

--- learnWebpack/19source-map/webpack.config.js
+++ learnWebpack/19source-map/webpack.config.js
@@ -75,4 +75,41 @@ module.exports = {
         //开启HMR功能
         hot: true
     },
+    devtool: 'source-map'
 }
+/*
+source-map: 一种提供源代码到构建后代码的映射技术
+            如果构建后代码出错可以追踪源代码错误
+            错误代码准确信息， 源代码的错误
+  [inline-|hidden-|eval-][nosources-][cheap-[module-]]source-map
+    inline-source-map: 内联，只生成一个内联的source-map 错误代码准确信息， 源代码的错误
+    hidden-source-map：外部 不能追踪源代码错误 不能追踪源代码错误，只能提示到构建后代码的错误位置
+    eval-souce-map: 内联, 每一个文件都生成对应的source-map，均在eval函数中 错误代码准确信息， 源代码的错误
+    nosources-source-map: 外部 错误代码准确信息,没有任何源代码信息
+    cheap-source-map: 外部  精确到行 错误代码准确信息， 源代码的错误位置
+    cheap-module-source-map: 外部  精确到行 错误代码准确信息， 源代码的错误位置
+                             会将loader的source-map加入
+内联跟外部的区别：
+    1.外部生成文件
+    2.内联构建速度更快
+
+
+    开发环境：
+
+    速度： eval>inline>cheap>...
+        eval-cheap-source-map
+        eval-source-map
+
+    调试：
+    source-map
+    cheap-module-source-map
+    cheap-source-map
+
+    ---》 eval-source-map / eval-cheap-module-source-map
+    生产环境：
+    内联会让代码体积变大，一般采用外部
+    none-source-map 全部隐藏
+    hidden-source-map 只隐藏源代码，会提示构建后代码
+
+    source-map
+ */
---
+++ learnWebpack/23code split/webpack.config.js
@@ -0,0 +1,127 @@
+const {resolve} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin  = require('optimize-css-assets-webpack-plugin')
+
+/*
+ */
+//定义Nodejs的环境变量：决定使用browserslist哪个环境
+process.env.NODE_ENV = 'production'
+
+//复用loader
+const commonCssLoader = [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    {
+        //需要在package.json中定义browserslist
+        loader: 'postcss-loader',
+        options: {
+            ident: 'postcss',
+            plugins: () => [
+                require('postcss-preset-env')()
+            ]
+        }
+    }
+]
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: "js/built.[contenthash:10].js",
+        path: resolve(__dirname,'build')
+    },
+    module: {
+        rules: [
+            {
+                //在package.json中eslintConfig -- airbnb
+                test: /\.js$/,
+                exclude: /node_modules/,
+                enforce: "pre",
+                loader: 'eslint-loader',
+                options: {
+                    fix: true
+                }
+            },
+            {
+                //以下loader只会选择一个，
+                //注意：不能同时匹配多个
+                oneOf: [
+                    {
+                        test: /\.css$/,
+                        use: [...commonCssLoader]
+                    },//css
+                    {
+                        test: /\.less$/,
+                        use: [
+                            ...commonCssLoader,
+                            'less-loader',
+                        ]
+                    },
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        loader: 'babel-loader',
+                        options: {
+                            presets:[
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        useBuiltIns: 'usage',
+                                        corejs:{
+                                            version: 3.14
+                                        },
+                                        targets: {
+                                            chrome: '60',
+                                            firefox: '50',
+
+                                        }
+                                    }
+                                ]
+                            ],
+                            //开启babel缓存，第二次构建时，会读取之前的缓存
+                            cacheDirectory: true
+                        }
+                    },
+                    {
+                        test: /\.(jpg|png|gif)/,
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8 * 1024,
+                            name: '[hash:10].[ext]',
+                            outputPath: 'img',
+                            esModule: false,
+                            publicPath: '../img'
+                        }
+                    },
+                    {
+                        test: /\.html$/,
+                        loader: 'html-loader',
+                    },
+                    {
+                        exclude: /\.(js|css|less|html|jpg|png|gif)/,
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'media'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/built.[contenthash:10].css'
+        }),
+        //压缩CSS
+        new OptimizeCssAssetsWebpackPlugin()
+    ],
+    mode: 'production',
+    devtool: 'source-map'
+}
