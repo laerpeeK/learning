93b66b0931f804eea6ac75efc2a1bedcf36074d9
laerpeeK <847749593@qq.com>
2021-06-25 23:52:51 +0800

    0625 把泛化this的过程提取出来

---
+++ learnJS/JavaScript设计模式与开发实践/闭包和高阶函数/08uncurrying.js
@@ -0,0 +1,12 @@
+//把泛化this的过程提取出来
+Function.prototype.uncurrying = function() {
+    var self = this
+    return function() {
+        var obj = Array.prototype.shift.call(arguments)
+        return self.apply(obj, arguments)
+    }
+}
+var push = Array.prototype.push.uncurrying()
+var a = {}
+push(a, 1,2)
+console.log(a) //{ '0': 1, '1': 2, length: 2 }
